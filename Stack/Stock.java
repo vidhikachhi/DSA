9f98bd1a866635b5bdfda7b99ed2483e9b88ec88
Vidhi Kachhi <118750257+vidhikachhi@users.noreply.github.com>
2025-07-29 18:15:47 +0530

    i agained changed the code just practicing the git commands

--- Stack/Stock.java
+++ Stack/Stock.java
@@ -1,7 +1,6 @@
 package Stack;
 import java.util.*;
-//i just used github command to edit and push back the code 
-//i did it again
+
 //Stock span problem
 //span means max no. of concecutive days for which
 // (price) <= (today's price)
